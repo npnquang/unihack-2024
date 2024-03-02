9613c12c2939363413da80a76934eb2f9a33cfce
NgoTranNgocDuy <ngotnduy@gmail.com>
2024-03-02 12:46:44 +1100

    test the base react

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
