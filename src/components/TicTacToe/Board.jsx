1b2ee8a67adda8d1ee525da35302cc0ad6249435
Pham Nhat Quang Nguyen <nguyenjoe0201@gmail.com>
2024-03-02 12:44:57 +1100

    Add TicTacToe

---
+++ src/components/TicTacToe/Board.jsx
@@ -0,0 +1,48 @@
+import { Square } from "./Square";
+import { PropTypes } from "prop-types"
+
+export function Board({ xIsNext, squares, onPlay, winner}) {
+  
+  function handleClick(i) {
+    if (squares[i] || winner) {
+      return
+    } 
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </>
+  );
+}
+
+Board.propTypes = {
+  xIsNext: PropTypes.bool.isRequired,
+  squares: PropTypes.array.isRequired,
+  onPlay: PropTypes.func.isRequired,
+  winner: PropTypes.string
+}
